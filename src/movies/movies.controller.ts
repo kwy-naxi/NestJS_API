d0843e93d2202860d5c889c5a71c77fe3a5822d8
kwy <kwy@KWYs-Machine.local>
2022-04-26 20:57:21 +0900

    test

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { Movie } from './entities/Movie.entity';
+import { MoviesService } from './movies.service';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(readonly moviesService: MoviesService) {}
+
+  @Get()
+  getAll() {
+    return this.moviesService.getAll();
+  }
+
+  // @Get('search')
+  // search(@Query('year') searchingYear: string) {
+  //   return `We are search title: ${searchingYear}`;
+  // }
+
+  @Get(':id')
+  getOne(@Param('id') movieId: number): Movie {
+    return this.moviesService.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() movieData: CreateMovieDto) {
+    return this.moviesService.create(movieData);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') movieId: number) {
+    return this.moviesService.deleteOne(movieId);
+  }
+
+  @Patch(':id')
+  path(@Param('id') movieId: number, @Body() updateDate: UpdateMovieDto) {
+    return this.moviesService.update(movieId, updateDate);
+  }
+}
---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,63 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+    await app.init();
+  });
+
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Welcome to my API page');
+  });
+
+  describe('/movies', () => {
+    it('GET', () => {
+      return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
+    });
+
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test1'],
+        })
+        .expect(201);
+    });
+
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/movies').expect(404);
+    });
+  });
+
+  // describe('/movies/:id', () => {
+  //   it('GET 200', () => {
+  //     return request(app.getHttpServer()).get('/movies/1').expect(200);
+  //   });
+  //   it('GET 404', () => {
+  //     return request(app.getHttpServer()).get('/movies/999').expect(404);
+  //   });
+  //   it.todo('DELETE');
+  //   it.todo('PATCH');
+  // });
+});
